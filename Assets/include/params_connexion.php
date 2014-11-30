52f10508d326b3d860230d5fc2b72d43bddbd078
Alexandre Marteaux <ialexandre.marteaux@gmail.com>
2014-11-30 18:55:53 +0100

    ajout de mes paramètres de connexion personnel

--- Assets/include/params_connexion.php
+++ Assets/include/params_connexion.php
@@ -1,13 +1,28 @@
-
 <?php
-if (! defined ('TEST_INCLUDE'))
-    die ("Vous ne pouvez pas accÃ©der directement Ã  ce fichier");
-/*$dns="mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw20148;";
-$user="dutinfopw20148";
-$password="upuryzyz";
+if (! defined ( 'TEST_INCLUDE' ))
+	die ( "Vous ne pouvez pas accÃ©der directement Ã  ce fichier" );
+
+//base de donnée de l'iut
+
+	/*
+ * $dns="mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw20148;";
+ * $user="dutinfopw20148";
+ * $password="upuryzyz";
+ */
+
+//base de donnée de brain-it.olympe.in
+
+/*
+$dns = "mysql:host=sql2.olympe.in;dbname=hsswbhsy;";
+$user = "hsswbhsy";
+$password = "brainitiut";
 */
-$dns="mysql:host=sql2.olympe.in;dbname=hsswbhsy;";
-$user="hsswbhsy";
-$password="brainitiut";
+
+//base de donné wamp (Alexandre Marteaux)
+
+ $dns="mysql:host=localhost;dbname=brainit;";
+ $user="root";
+ $password="";
+
 
 ?>
