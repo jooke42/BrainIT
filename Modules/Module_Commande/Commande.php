11cc3f4e30852d5b7167471f46495c242d646fac
ghezali-brahim <ghezali.brahim@free.fr>
2015-02-09 00:52:18 +0100

    Update Commande by Brahim & Aurelien

--- Modules/Module_Commande/Commande.php
+++ Modules/Module_Commande/Commande.php
@@ -1,40 +1,55 @@
 <?php
-if (!defined('TEST_INCLUDE')) {
-    throw new Exception ("Vous ne pouvez pas acceder directement � ce fichier");
+if ( !defined ( 'TEST_INCLUDE' ) ) {
+    throw new Exception ( "Vous ne pouvez pas acceder directement � ce fichier" );
 }
-include_once("Controleur_" . $module . "/controleur_" . $module . ".php");
-include_once("Vue_" . $module . "/vue_" . $module . ".php");
-include_once("Modele_" . $module . "/modele_" . $module . ".php");
-
-class Commande extends Module {
-
-    function __construct () {
+include_once ( "Controleur_" . $module . "/controleur_" . $module . ".php" );
+include_once ( "Vue_" . $module . "/vue_" . $module . ".php" );
+include_once ( "Modele_" . $module . "/modele_" . $module . ".php" );
 
 
-        $module = get_class($this);
+class Commande extends Module
+{
+
+    function __construct ()
+    {
+        $module        = get_class ( $this );
         $nomControleur = 'Controleur' . $module;
-        $monControleur = new $nomControleur($module);
-
-        if (!isset($action)) {
-
-            if (isset($_GET['action'])) {
-                $action = $_GET['action'];
+        $monControleur = new $nomControleur( $module );
+        if ( !isset( $action ) ) {
+            if ( isset( $_GET[ 'action' ] ) ) {
+                $action = $_GET[ 'action' ];
             } else {
                 $action = 0;
             }
         }
-        switch ($action) {
-            case 0:
-                $monControleur->creationCommande();
-                break;
+
+        switch ( $action ) {
             case 1:
-                //$monControleur->envoieMontantPaiement($idCommande);//TODO $idCommande inconnu
+                //$monControleur->creationCommande();
                 break;
             case 2:
-                $monControleur->recuConfirmation();
+                // $monControleur->recuConfirmation();
+                break;
+            case "affichageFinCommande":
+                $monControleur->affichageFinCommande ();
+                break;
+            case "creationCommande":
+                $monControleur->creationCommande ();
+                break;
+            case "envoieFournisseur":
+                $monControleur->envoieFournisseur ();
+                break;
+            case "envoieBanque":
+                $monControleur->envoieBanque ();
+                break;
+            case "setDateLivraison":
+                $monControleur->setDateLivraison ();
+                break;
+            case "ConnexionLivraison":
+                $monControleur->connexionLivraison ();
                 break;
             default:
-                $monControleur->afficherCommande();
+                // $monControleur->afficherCommande();
                 break;
         }
     }
