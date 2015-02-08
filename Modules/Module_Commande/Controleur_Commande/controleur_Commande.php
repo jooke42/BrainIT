e2ab5963ef9041929a4ee64acb452fbbbf9fdb2d
ghezali-brahim <ghezali.brahim@free.fr>
2015-02-09 00:54:16 +0100

    Update Commande by Brahim & Aurelien

--- Modules/Module_Commande/Controleur_Commande/controleur_Commande.php
+++ Modules/Module_Commande/Controleur_Commande/controleur_Commande.php
@@ -1,44 +1,188 @@
 <?php
 
-class ControleurCommande {
+
+class ControleurCommande
+{
 
 
+    public  $idCommande;
     private $maVue;
     private $monModele;
 
-    function __construct ($module) {
-
-        $nomVue = 'Vue' . $module;
+    function __construct ( $module )
+    {
+        $nomVue    = 'Vue' . $module;
         $nomModele = 'Modele' . $module;
-        $idCommande=ModeleCommande::getLastIdCommande($_SESSION['idClient']);
-        $this->monModele = new $nomModele($idCommande);
-        $this->maVue = new $nomVue();
-
-    }
-
-
-    function afficherCommande () {
-        $req = $this->monModele->afficherCommande();
-        $this->maVue->afficherCommande($req);
-    }
-
-    function envoieMontantTotal($idCommande){
-        //$montant=$this->monModele->getMontantChaqueArticle();
-        $montant=ModeleCommande::getMontantChaqueArticle($idCommande);
-        $somme =0;
-        foreach($articles as $article){
-	      $somme+=$article['montantArticle'];
+        if ( isset( $_GET[ 'idCommande' ] ) ) {
+            $idCommande      = htmlspecialchars ( $_GET[ 'idCommande' ] );
+            $this->monModele = new $nomModele( $idCommande );
         }
-        return $somme;
+        $this->maVue = new $nomVue();
     }
 
-    function creationCommande(){
-        ModeleCommande::creationCommande($_SESSION['idClient']);
-	//$idCommande =ModeleCommande::createCommande($_SESSION['idClient']);
-      //  $somme=self::envoieMontantTotal($idCommande);
-        //header("Location: http://amfbank.esy.es/METHODOPROJET/local_AMF/index.php?module=APIPaiement&action=afficherPopup&id=$idCommande&montant=$somme&nom=BRAINIT");
-
+    function setDateLivraison ()
+    {
+        $this->setModele ();
+        echo $_POST[ 'dateLivraison' ];
+        $this->monModele->validerLivraison ( $_POST[ 'dateLivraison' ] );
     }
 
+    function setModele ()
+    {
+        $this->monModele = new ModeleCommande( ModeleCommande::getLastIdCommande ( $_SESSION[ 'idClient' ] ) );
+    }
+
+    function envoieBanque ()
+    {
+        $this->setModele ();
+        if ( !$this->monModele->getStatus () ) {
+            $this->paiementCommande ();
+        } else {
+            header('Location: index.php?Module=Commande&action=envoieFournisseur');
+        }
+    }
+
+    function paiementCommande ()
+    {
+        $this->setModele ();
+        if ( isset( $_POST[ 'validation' ] ) ) {
+            if ( $_POST[ 'validation' ] == TRUE ) {
+                $this->monModele->validerPaiementCommande ();
+            } else {
+                echo "Echec du paiement";
+            }
+        } else {
+            //TODO modifier le lien
+            ?>
+            <body onload="submit()" >
+
+
+            <form action="#" method="post" id="form" >
+                <input type="hidden" name="montant" value='<?= $this->monModele->getMontantTotal () ?>' >
+                <input type="hidden" name="url" value="<?= ModeleCommande::$urlPaiement ?>" />
+                <input type="submit" value="envoyer" >
+            </form >
+            <script type="text/javascript" >
+                function submit() {
+                    var form = document.getElementById('form');
+                    form.submit();
+                }
+            </script >
+            </body >
+
+        <?php
+        }
+    }
+
+    /**
+     * Fonction pour appeller la livraison
+     */
+    function envoieFournisseur ()
+    {
+        $this->setModele ();
+        if ( $this->monModele->getDateLivraison () == NULL ) {
+            if ( isset( $_SESSION[ 'token' ] ) ) {
+                // echo"J'ai déjà une session avec un token";
+                $this->commandeLivraison ();
+            } else if ( $_POST[ 'token' ] ) {
+                $_SESSION[ 'token' ] = $_POST[ 'token' ];
+                // echo"Je reçoit un token";
+                $this->commandeLivraison ();
+            } else {
+                //echo"Je me connecte SWAG";
+                $this->connexionLivraison ();
+            }
+        } else {
+            $this->affichageFinCommande ();
+        }
+    }
+
+    function commandeLivraison ()
+    {
+        $this->setModele ();
+        $url = ModeleCommande::$urlLivraison;
+        if ( isset( $_SESSION[ 'token' ] ) ) {
+            echo '<body onload="submit()">';
+            ?>
+
+            <form action="http://pageperso.iut.univ-paris8.fr/~jdasilva/Metho/API/API_commande.php" method="post"
+                  id="form" >
+                <input type="hidden" name="token" value="<?= $_SESSION[ 'token' ] ?>" >
+                <input type="hidden" name="commande" value='<?= $this->monModele->getJsonLivraison () ?>' >
+                <input type="hidden" name="url" value="<?= $url ?>" />
+                <input type="submit" value="envoyer" >
+            </form >
+            <?php
+            echo '
+    <script type="text/javascript">
+     function submit(){
+      var form = document.getElementById(\'form\');
+      form.submit();
+     }
+    </script>
+   </body>
+   ';
+        } else {
+            $this->connexionLivraison ();
+        }
+    }
+
+    /**
+     * Definit la variable de session du token
+     */
+    function connexionLivraison ()
+    {
+        if ( isset( $_POST[ 'token' ] ) ) {
+            $_SESSION[ 'token' ] = $_POST[ 'token' ];
+            echo "Connexion au service de livraison reussit !";
+        } else if ( !isset( $_SESSION[ 'token' ] ) ) {
+            echo '<body onload="submit()">
+        <form action="http://pageperso.iut.univ-paris8.fr/~jdasilva/Metho/API/API_connexion.php" method="post" id="form">
+                <input type="hidden" name="nomBoutique" value="BrainIT"/>
+                <input type="password" name="password" value="' . md5 ( 'nbinqu9b8bu1jk6xyvu5jl62r' ) . '" />
+                <input type="hidden" name="url" value ="' . ModeleCommande::$urlLivraison . '" />.
+                <input type="submit" value="Envoyer"/>
+                </form>
+   ';
+            echo '
+    <script type="text/javascript">
+     function submit(){
+      var form = document.getElementById(\'form\');
+      form.submit();
+     }
+    </script>
+   </body>
+   ';
+        }
+    }
+
+    function affichageFinCommande ()
+    {
+        $this->setModele ();
+        $information = $this->monModele->getRecapCommande ();
+        $this->maVue->affichageFinalCommande ( $information );
+    }
+
+    /**
+     * Vide le panier et créer une commande seulement si commande précedente confirmé
+     */
+    function creationCommande ()
+    {
+        if ( !isset( $_SESSION[ 'idClient' ] ) ) {
+            die( "veuillez vous connecter" );
+        }
+        if ( ModeleCommande::getLastIdCommande ( $_SESSION[ 'idClient' ] ) == NULL ) {
+            ModeleCommande::creationCommande ( $_SESSION[ 'idClient' ] );
+            header('Location: index.php?Module=Commande&action=envoieBanque');
+        } else {
+            $this->setModele ();
+            if ( $this->monModele->getStatus () == TRUE && $this->monModele->getDateLivraison () != NULL ) {
+                ModeleCommande::creationCommande ( $_SESSION[ 'idClient' ] );
+                header('Location: index.php?Module=Commande&action=envoieBanque');
+            } else {
+                die( "Veuillez finir de confirmer la commande précédente avant d'en créer une nouvelle ; id Commande :" . ModeleCommande::getLastIdCommande ( $_SESSION[ 'idClient' ] ) );
+            }
+        }
+    }
 }
 
