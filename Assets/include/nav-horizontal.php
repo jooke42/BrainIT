b21ad60650200bf6071f80ecfb7cdb89a20f7672
Alexandre Marteaux <ialexandre.marteaux@gmail.com>
2015-02-08 16:52:43 +0100

    ajout déco

--- Assets/include/nav-horizontal.php
+++ Assets/include/nav-horizontal.php
@@ -66,7 +66,14 @@
                             <!--<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>-->
                             <li><a href="index.php?Module=Panier&action=0"><i class="fa fa-shopping-cart"></i>
                                     Panier</a></li>
-                            <li><a href="index.php?Module=Connexion"><i class="fa fa-lock"></i> Connexion</a></li>
+                            <?php
+                            if(!isset($_SESSION['Utilisateur'])){
+                                echo '<li><a href="index.php?Module=Connexion"><i class="fa fa-lock"></i> Connexion</a></li>';
+                            }else{
+                                echo '<li><a href="Assets/include/deconnexion.php"><i class="fa fa-lock"></i> Deconnexion</a></li>';
+                            }
+
+                            ?>
                         </ul>
                     </div>
                 </div>
