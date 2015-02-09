f83520fd0b703dccbe1333192aa8638a31fea96e
ghezali-brahim <ghezali.brahim@free.fr>
2015-02-09 12:39:44 +0100

    Update modele_Commande.php

--- Modules/Module_Commande/Modele_Commande/modele_Commande.php
+++ Modules/Module_Commande/Modele_Commande/modele_Commande.php
@@ -6,7 +6,7 @@ if ( !defined ( 'TEST_INCLUDE' ) )
 
 class ModeleCommande extends DBMapper
 {
-    static    $urlPaiement  = "http://brainit.esy.es/index.php?Module=Commande&action=envoiePaiement"; // URL de retour de la banque
+    static    $urlPaiement  = "http://brainit.esy.es/index.php?Module=Commande&action=envoieBanque"; // URL de retour de la banque
     static    $urlLivraison = "http://brainit.esy.es/index.php?Module=Commande&action=envoieFournisseur"; // URL de retour de la livraison protected $idCommande;
     protected $status;// Etat paiement
     protected $dateLivraison;//Etat livraison
